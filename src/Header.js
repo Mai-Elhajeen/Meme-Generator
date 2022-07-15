147a6b00c8710d992622c306805fcbaa93a2e9c2
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-07-15 11:57:42 +0300

    adding html code for header

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <div className='left-side'>
+        <img src='./img/Trollface_non-free.png' />
+        <h1>Meme Generator</h1>
+      </div>
+
+      <div className='right-side'>
+        <p>React Course - Project 8</p>
+      </div>
+    </header>
   )
 }
 
